1117f35c50bd9407a758114bc79364a90fb2bb3a
lifeoncode <ndu918@outlook.com>
2020-10-20 15:58:02 +0200

    page transitions done

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ window.addEventListener('DOMContentLoaded', function(e){
     const [menuBtn, hamburger, menuDiv] = [document.querySelector('.menu_btn'), document.querySelectorAll('.menu_btn span'), document.querySelector('.menu')];
     // menu links
     const [homeLink, aboutLink, lessonsLink, nextLink] = [document.querySelector('.home_link'), document.querySelector('.about_link'), document.querySelector('.lessons_link'), document.querySelector('.next_link')];
+    // all the links
+    const [...links] = [homeLink, aboutLink, lessonsLink, nextLink];
+
+    links.forEach(link => {
+        link.addEventListener('click', closeMenu);
+    })
 
     // events
     menuBtn.addEventListener('click', openMenu);
@@ -51,9 +57,63 @@ window.addEventListener('DOMContentLoaded', function(e){
         menuDiv.classList.remove('open');
         // grab all the menu links
         // hide at once
-        let [...links] = [homeLink, aboutLink, lessonsLink, nextLink];
         links.forEach(link => {
             link.classList.remove('open');
         })
     }
+
+
+
+
+
+    // section changes
+    const [home, about, lessons, next] = [document.querySelector('.home'), document.querySelector('.about'), document.querySelector('.lessons'), document.querySelector('.next')];
+
+    // timeline
+    const [one, two, three, four] = [document.querySelector('.one'), document.querySelector('.two'), document.querySelector('.three'), document.querySelector('.four')]
+    
+    homeLink.addEventListener('click', (e) => {
+        home.style.transform = `translateY(0%)`;
+        about.style.transform = `translateY(100%)`;
+        lessons.style.transform = `translateY(100%)`;
+        next.style.transform = `translateY(100%)`;
+
+        one.style.opacity = '1';
+        two.style.opacity = '0.2';
+        three.style.opacity = '0.2';
+        four.style.opacity = '0.2';
+    })
+    aboutLink.addEventListener('click', (e) => {
+        home.style.transform = `translateY(-100%)`;
+        about.style.transform = `translateY(0%)`;
+        lessons.style.transform = `translateY(100%)`;
+        next.style.transform = `translateY(100%)`;
+
+        one.style.opacity = '0.2';
+        two.style.opacity = '1';
+        three.style.opacity = '0.2';
+        four.style.opacity = '0.2';
+    })
+    lessonsLink.addEventListener('click', (e) => {
+        home.style.transform = `translateY(-100%)`;
+        about.style.transform = `translateY(-100%)`;
+        lessons.style.transform = `translateY(0%)`;
+        next.style.transform = `translateY(100%)`;
+
+        one.style.opacity = '0.2';
+        two.style.opacity = '0.2';
+        three.style.opacity = '1';
+        four.style.opacity = '0.2';
+    })
+    nextLink.addEventListener('click', (e) => {
+        home.style.transform = `translateY(-100%)`;
+        about.style.transform = `translateY(-100%)`;
+        lessons.style.transform = `translateY(-100%)`;
+        next.style.transform = `translateY(0%)`;
+
+        one.style.opacity = '0.2';
+        two.style.opacity = '0.2';
+        three.style.opacity = '0.2';
+        four.style.opacity = '1';
+    })
 })
